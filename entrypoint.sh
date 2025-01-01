cea73f5435b87ee130bb7f3e789ec699dc5fd23f
Chad Crouch <chad.crouch90@gmail.com>
2025-01-01 13:28:54 +0200

    fix - adjust docker permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ fi
 
 RUNNER_NAME=${RUNNER_NAME:-"default-runner"}
 
+sudo service docker start
+
 # Trap SIGTERM and SIGINT to allow for cleanup
 trap './config.sh remove --unattended && exit 0' SIGTERM SIGINT
 
